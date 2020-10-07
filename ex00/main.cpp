0cf4daaa0d06b63c30b096be7644c0e5414513d2
tblaudez <tblaudez@student.42.fr>
2020-10-07 15:03:35 +0200

    TODO: find out why ex00 always return same rand() value

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,25 +6,26 @@
 /*   By: tblaudez <tblaudez@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/23 16:56:45 by tblaudez      #+#    #+#                 */
-/*   Updated: 2020/09/24 16:26:15 by tblaudez      ########   odam.nl         */
+/*   Updated: 2020/10/07 15:02:56 by tblaudez      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdlib>
 #include "FragTrap.hpp"
+
+#include <cstdlib> // srand
+#include <ctime> // time
 
 int	main(void) {
 
 	srand(time(0));
 
-	FragTrap	frag("Fraggy");
-	std::cout << "------------" << std::endl;
+	FragTrap	frag("FR4G-TP");
+
 	frag.rangedAttack("some skags");
 	frag.meleeAttack("Jack himself");
 	frag.takeDamage(9999);
 	frag.beRepaired(50);
 	frag.vaulthunter_dot_exe("The Vault Hunter");
 
-	std::cout << std::endl;
 	return 0;
 }
